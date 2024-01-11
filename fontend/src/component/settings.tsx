5eda29ae0ba2399ea529568d2af7f311f65231b9
Thomas Syben <thomassyben07@gmail.com>
2024-01-11 15:47:55 +0100

    add changes on setting component

--- fontend/src/component/settings.tsx
+++ fontend/src/component/settings.tsx
@@ -6,10 +6,11 @@ const Settings: React.FC = () => {
     const [profileImageUrl, setProfileImageUrl] = useState<string>(image);
     const [email, setEmail] = useState('test@email.com');
     const [name, setName] = useState('Nom');;
-    const [bio, setBio] = useState('');
-    const [password, setPassword] = useState('');
-    const [country, setCountry] = useState('');
-    const [age, setAge] = useState('');
+    const [bio, setBio] = useState('Ceci est une BIO');
+    const [password, setPassword] = useState('password');
+    const [repeatPassword, setRepeatPassword] = useState('password');
+    const [country, setCountry] = useState('Belgium');
+    const [age, setAge] = useState('21');
 
     const handleImageChange = (e: React.ChangeEvent<HTMLInputElement>) => {
         if (e.target.files) {
@@ -22,31 +23,46 @@ const Settings: React.FC = () => {
     const handleEmailChange = (e: React.ChangeEvent<HTMLInputElement>) => {
         setEmail(e.target.value);
     };
+    const handleBioChange = (e: React.ChangeEvent<HTMLTextAreaElement>) => {
+        setBio(e.target.value);
+    };
+    const handlePasswordChange = (e: React.ChangeEvent<HTMLInputElement>) => {
+        setPassword(e.target.value);
+    };
+    const handleRepeatPasswordChange = (e: React.ChangeEvent<HTMLInputElement>) => {
+        setRepeatPassword(e.target.value);
+    };
+    const handleCountryChange = (e: React.ChangeEvent<HTMLInputElement>) => {
+        setCountry(e.target.value);
+    };
+    const handleAgeChange = (e: React.ChangeEvent<HTMLInputElement>) => {
+        setAge(e.target.value);
+    }; 
 
     return (
         <div className="flex justify-center items-center h-screen">
             <form className="flex flex-col items-center">
-                <div className="flex flex-row">
-                    <h1>SETTINGS</h1>
+                <div className="flex flex-col items-center my-3">
+                    <h1 className="py-3 text-white">SETTINGS</h1>
                     <SaveButton />
                 </div>
                 <div className="flex flex-row">
-                    <div className="flex flex-col items-center">
+                    <div className="flex flex-col items-center mx-2">
                         <img src={profileImageUrl} alt="Profile" className="w-20 h-20 object-cover rounded-full" />
                         <input type="file" onChange={handleImageChange} className="border p-2" />
                     </div>
-                    <div className="flex flex-col items-center">
-                        <input type="text" value={name} onChange={(handleNameChange)} className="border p-2" defaultValue="test"/>
-                        <textarea value={bio} onChange={(e) => setBio(e.target.value)} className="border p-2" placeholder="Bio" />
+                    <div className="flex flex-col items-center mx-2">
+                        <input type="text" value={name} onChange={(handleNameChange)} className="border-b-2 p-2 focus:outline-none bg-transparent text-white rounded-sm" defaultValue="test"/>
+                        <textarea value={bio} onChange={(handleBioChange)} className="border p-2 focus:outline-none bg-transparent text-white rounded-sm" placeholder="Bio" />
                     </div>
-                    <div className="flex flex-col items-center">
-                        <input type="email" value={email} onChange={handleEmailChange} className="border p-2" placeholder="Email" />
-                        <input type="password" value={password} onChange={(e) => setPassword(e.target.value)} className="border p-2" placeholder="Mot de passe" />
-                        <input type="password" className="border p-2" placeholder="Réinitialiser le mot de passe" />
+                    <div className="flex flex-col items-center mx-2">
+                        <input type="email" value={email} onChange={handleEmailChange} className="border-b-2 p-2 focus:outline-none bg-transparent text-white rounded-sm" placeholder="Email" />
+                        <input type="password" value={password} onChange={handlePasswordChange} className="border-b-2 p-2 focus:outline-none bg-transparent text-white rounded-sm" placeholder="Mot de passe" />
+                        <input type="password" value={repeatPassword} onChange={handleRepeatPasswordChange} className="border-b-2 p-2 focus:outline-none bg-transparent text-white rounded-sm" placeholder="Réinitialiser le mot de passe" />
                     </div>
-                    <div className="flex flex-col items-center">
-                        <input type="text" value={country} onChange={(e) => setCountry(e.target.value)} className="border p-2" placeholder="Pays" />
-                        <input type="number" value={age} onChange={(e) => setAge(e.target.value)} className="border p-2" placeholder="Âge" />
+                    <div className="flex flex-col items-center mx-2">
+                        <input type="text" value={country} onChange={handleCountryChange} className="border-b-2 p-2 focus:outline-none bg-transparent text-white rounded-sm" placeholder="Pays" />
+                        <input type="number" value={age} onChange={handleAgeChange} className="border-b-2 p-2 focus:outline-none bg-transparent text-white rounded-sm" placeholder="Âge" />
                         <DeleteButton />
                     </div>
                 </div>
